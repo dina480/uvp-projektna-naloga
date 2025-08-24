af2e31f5c5a3aa0ca1400aceec3013793bb48c4d
dina480 <dg42442@student.uni-lj.si>
2025-08-24 09:35:37 +0200

    Začetna nastavitev projekta in uvoz paketov

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -6,3 +6,13 @@ import csv
 import time
 
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import undetected_chromedriver as uc
+
+# URL spletne strani, kjer se nahajajo rezultati
+BASE_URL = "https://www.nepremicnine.net/oglasi-oddaja/stanovanje/soba/{page}/"
+CSV_FILE = "rezultati.csv"
+MAX_CONSECUTIVE_EMPTY = 3
